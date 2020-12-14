63eb59b851821a267712c06b81bdc8f9af8fef23
Carlos Diaz <diazpolanco13@gmail.com>
2020-12-14 02:03:27 -0400

    header en progreso

--- components/landing/Layout.js
+++ components/landing/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from './Hero'
 import Navbar from './Navbar'
 
 const Layout = ({ children }) => {
     return (
         <>
-        <Navbar/>
+            <Navbar />
+                {/* <Hero/> */}
             { children }
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Hero from '../components/landing/Hero'
 import Layout from '../components/landing/Layout'
 // import Sping from '../components/Sping'
 
@@ -7,6 +8,7 @@ const index = () => {
   return (
     <>
       <Layout>
+        <Hero />
       <h1>Bienvenido a Vital Solution Store</h1>
 
       </Layout>
