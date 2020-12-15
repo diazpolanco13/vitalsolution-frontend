c1e2917ff6007ffa223d7530c1f516d006337d03
Carlos Diaz <diazpolanco13@gmail.com>
2020-12-15 17:42:57 -0400

    carouserl funcional en local

--- components/landing/Carrucel.js
+++ components/landing/Carrucel.js
@@ -15,9 +15,6 @@ const items = [
     <div className="item" data-value="1">
         <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://firebasestorage.googleapis.com/v0/b/vital-solution-store.appspot.com/o/banner%203.jpg?alt=media&token=76fdb4f6-4631-4426-99fd-510de3e29bef" />
     </div>,
-    // <div className="item" data-value="2">
-    //     <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://firebasestorage.googleapis.com/v0/b/vital-solution-store.appspot.com/o/bannervital.jpg?alt=media&token=98dfe3f7-7bfd-4e4f-b651-6bb6de6e6f92" />
-    // </div>
 ];
 
 const Carrucel = () => {
