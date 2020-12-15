00836d595964c91b5a2dac785447a88c1b8a13d9
Carlos Diaz <diazpolanco13@gmail.com>
2020-12-15 18:57:08 -0400

    fotos a cloudinery

--- components/landing/Carrucel.js
+++ components/landing/Carrucel.js
@@ -10,10 +10,13 @@ const responsive = {
 
 const items = [
     <div className="item" data-value="0">
-        <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://firebasestorage.googleapis.com/v0/b/vital-solution-store.appspot.com/o/bannner1.jpg?alt=media&token=f3e2e2da-5822-43e1-9b31-156051487376" />
+        <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://res.cloudinary.com/apidev/image/upload/v1608072900/UI/nwwbyjtza0ernfffww9c.jpg" />
     </div>,
     <div className="item" data-value="1">
-        <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://firebasestorage.googleapis.com/v0/b/vital-solution-store.appspot.com/o/banner%203.jpg?alt=media&token=76fdb4f6-4631-4426-99fd-510de3e29bef" />
+        <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://res.cloudinary.com/apidev/image/upload/v1608072892/UI/srxvt9par1ftuexiroxq.jpg" />
+    </div>,
+    <div className="item" data-value="1">
+        <img className="object-cover object-center w-full rounded h-96" alt="hero" src="https://res.cloudinary.com/apidev/image/upload/v1608072927/UI/q2mhhgzknp9a2e38hiwb.jpg" />
     </div>,
 ];
 
